e9c5964c19dd870184d0c28261dd707faf376396
chuwong35122 <chuwong35122@gmail.com>
2021-12-22 19:19:16 +0700

    Hello World from Go!

---
+++ hello_world.go
@@ -0,0 +1,22 @@
+/*
+Amazing tutorial here!
+https://fireship.io/lessons/learn-go-in-100-lines/
+*/
+
+package main // required for standalone executable
+import "fmt" // implements formatted IO
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+/*
+Build file using
+$ go build file_name.go
+
+The result is executable file
+Run it using $ ./file_name
+
+OR (this command will not create .exe file)
+$ go run file_name.go
+*/
